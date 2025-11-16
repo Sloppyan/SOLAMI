777df50e14b75aa53f56227e856bf8c833ec64dd
HsinyuG <xavier_gao@protonmail.com>
2025-11-16 17:39:37 +0100

    fix: comment some manipulation of  example_data

--- datasets/motion_dataset/raw_process_humanml3d_2_unified.py
+++ datasets/motion_dataset/raw_process_humanml3d_2_unified.py
@@ -765,8 +765,8 @@ if __name__ == "__main__":
     # Get offsets of target skeleton
     # we random choose one
     example_data = np.load('SOLAMI_data/HumanML3D/HumanML3D_no_mirror/000021.npy')
-    example_data = example_data['pose_np']
-    example_data = example_data[:, body_joints_id + hand_joints_id, :]
+    # example_data = example_data['pose_np']
+    # example_data = example_data[:, body_joints_id + hand_joints_id, :]
     example_data = example_data.reshape(len(example_data), -1, 3)
     example_data = torch.from_numpy(example_data)
 
